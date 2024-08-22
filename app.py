ad20ba46cfe1c8f6c9d2fc44cd81b06fdea9bbc8
Lakshman <48505442+LakshmanHA@users.noreply.github.com>
2024-08-22 07:06:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import validators,streamlit as st
+import validators
+import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
